db889b04f6afb5859ec57cde0784556f0485521a
Moses Esan <esanm@rte.ie>
2020-04-29 02:09:37 +0100

    Added OverlayContainer Component

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {Placeholder, Loading, Error, Empty, Header, Footer, NavIcon} from './ListHelpers/ListHelpers';
+import {Placeholder, Loading, Error, Empty, Header, Footer, NavIcon, OverlayContainer} from './ListHelpers/ListHelpers';
 import FilterView from './FilterView';
 import Panel from './Panel';
 
-export { Placeholder, Loading, Error, Empty, Header, Footer, NavIcon, FilterView, Panel };
+export { Placeholder, Loading, Error, Empty, Header, Footer, NavIcon, FilterView, Panel, OverlayContainer };
